7c8c2b86e17d457241a4c401c535b3591f0b4d63
Victoria Ng <victoria.ng555@gmail.com>
2021-06-17 23:09:27 -0500

    One extension for sharing profile

--- Poshmarrk-Auto-Share/background.js
+++ Poshmarrk-Auto-Share/background.js
@@ -1,16 +1,53 @@
 browser.browserAction.onClicked.addListener((tab_obj) => {
-  console.log(tab_obj.url);
+  let redirect = false;
+  let newUrl = "";
+  if (tab_obj.url.indexOf("?") == -1) {
+    redirect = true
+    const concatItem = tab_obj.url.indexOf("?") == -1 ? "?" : "&";
+    newUrl = tab_obj.url + "?department=Women&availability=available";
+  } else {
+    const params = ((ab)=>{
+      return ab.split("?")[1].split("&").reduce((map, obj) => {
+        map[obj.split("=")[0]] = obj.split("=")[1]; return map;
+      }, {});
+    })(tab_obj.url);
+    console.table(params);
 
-  // const concatItem = tab_obj.url.indexOf("?") == -1 ? "?" : "&";
-  // const tabLocation = tab_obj.url + concatItem + "department=Women&availability=available";
+    if (params.propertyIsEnumerable("department")) {
+      if (params.department != "Women") {
+        redirect = true;
+      }
+      delete params.department;
+    } else {
+      redirect = true;
+    }
 
-  // var updating = browser.tabs.update(tab_obj.id, {url: tabLocation})
-  // updating.then((tab_obj) => {
-  //   console.log("Starting content.js...")
-  //   browser.tabs.executeScript(tab_obj.id, {file: 'content.js', runAt: 'document_idle'});
-  // }, (tab_obj) => {
-  //   console.error("Failed to navigate.");
-  // });
+    if (params.propertyIsEnumerable("availability")) {
+      if (params.availability != "available") {
+        redirect = true;
+      }
+      delete params.availability;
+    } else {
+      redirect = true;
+    }
+    console.log(params);
 
-  browser.tabs.executeScript(tab_obj.id, {file: 'content.js', runAt: 'document_idle'});
+    const remainingParams = Object.getOwnPropertyNames(params).map((i) => {return i + "=" + params[i]}).join("&");
+    console.log(remainingParams);
+    newUrl = tab_obj.url.split("?")[0] + "?department=Women&availability=available&" + remainingParams;
+    console.log(newUrl);
+  }
+
+  if (redirect) {
+    console.log(`Navigating to URL: ${newUrl}...`)
+    var updating = browser.tabs.update(tab_obj.id, {url: newUrl})
+    updating.then((tab_obj) => {
+      console.log("Navigated to available women's items");
+    }, (tab_obj) => {
+      console.error("Failed to navigate.");
+    });
+  } else {
+    console.log(`Skipping navigation. Sharing closet...`);
+    browser.tabs.executeScript(tab_obj.id, {file: 'content.js', runAt: 'document_idle'});
+  }
 });
