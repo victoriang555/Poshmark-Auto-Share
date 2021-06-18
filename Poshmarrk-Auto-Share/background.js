367157eb7f6783dda54bd0a7992e8a488e1d0fbb
Victoria Ng <victoria.ng555@gmail.com>
2021-06-17 22:21:51 -0500

    Add step 1

--- Poshmarrk-Auto-Share/background.js
+++ Poshmarrk-Auto-Share/background.js
@@ -1,16 +1,13 @@
 browser.browserAction.onClicked.addListener((tab_obj) => {
   console.log(tab_obj.url);
 
-  // const concatItem = tab_obj.url.indexOf("?") == -1 ? "?" : "&";
-  // const tabLocation = tab_obj.url + concatItem + "department=Women&availability=available";
+  const concatItem = tab_obj.url.indexOf("?") == -1 ? "?" : "&";
+  const tabLocation = tab_obj.url + concatItem + "department=Women&availability=available";
 
-  // var updating = browser.tabs.update(tab_obj.id, {url: tabLocation})
-  // updating.then((tab_obj) => {
-  //   console.log("Starting content.js...")
-  //   browser.tabs.executeScript(tab_obj.id, {file: 'content.js', runAt: 'document_idle'});
-  // }, (tab_obj) => {
-  //   console.error("Failed to navigate.");
-  // });
-
-  browser.tabs.executeScript(tab_obj.id, {file: 'content.js', runAt: 'document_idle'});
+  var updating = browser.tabs.update(tab_obj.id, {url: tabLocation})
+  updating.then((tab_obj) => {
+    console.log("Navigated to available women's items");
+  }, (tab_obj) => {
+    console.error("Failed to navigate.");
+  });
 });
